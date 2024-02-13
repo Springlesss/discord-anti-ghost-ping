21cd52a2e33345a89e00c1cd9a2e3c24bd6a7e6a
Springles <104740481+Springlesss@users.noreply.github.com>
2024-02-13 09:19:25 +0100

    uploaded the initial 1.0.0 files

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const config = require('./config.json');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+    console.log('The bot is ready to go!')
+});
+
+client.on('messageDelete', message => {
+    if (message.mentions.users.size > 0) {
+        const embed = new EmbedBuilder()
+        .setTitle('**Ghost ping detected!**')
+        .setDescription(`Message: \n ${message.content} \n \n The user who ghost pinged: ${message.author}`)
+        .setTimestamp()
+        .setThumbnail('https://cdn3.emoji.gg/stickers/1127-ping.png')
+        .setColor('Red')
+
+        const alertChannel = message.guild.channels.cache.get(config.alertChannelId);
+        alertChannel.send({ embeds: [embed] });
+
+        if (config.timeout == true) {
+            message.member.timeout(config.timeoutMinutes * 60 * 1000).catch(() => console.log('The bot is missing permissions!'))
+        }
+    }
+});
+
+client.login(config.token);
